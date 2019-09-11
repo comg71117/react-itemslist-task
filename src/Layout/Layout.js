122fbe1dba110fd1b1145c458693b40b5a271126
Georgy Slavev <g.slavev@attractsoft.com>
2019-09-11 11:48:18 +0300

    matterial-ui added and works

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 export default class App extends React.Component {
 
@@ -6,6 +8,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <div>{this.props.title}</div>
+                <Button variant="outlined" />
             </div>
             // <div>
             //     <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
